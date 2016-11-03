3c0fb785a1f01bbb9ba12448f052a58ee2fac9a4
root <root@StagingTwo.iumdxofgcvhe5glccu5e251moe.zx.internal.cloudapp.net>
2016-11-03 15:45:03 +0000

    Added script for automating database backup

--- webapp/backup.sh
+++ webapp/backup.sh
@@ -1,1 +1,3 @@
-s3cmd -r sync backup s3://database-backup-scenario-week-3
+#!/bin/bash
+
+s3cmd -r put /home/teamu/AWS_VM_References/webapp/backup/ s3://database-backup-scenario-week-3/backup-$(date +%d-%m-%Y/%H:%M:%S)/ > /var/log/myjob.log 2>&1
