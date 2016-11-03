9a73a11c9ed5515c02d160aa4419e72920d8c714
Pranav Nashikkar <pnashikkar96@gmail.com>
2016-11-03 16:04:22 +0000

    git pull
    trying to get the updates on the branch

--- scripts/git_deploy.sh
+++ scripts/git_deploy.sh
@@ -4,6 +4,7 @@ ssh app@51.140.162.169 <<EOF
   kill -9 $(lsof -ti tcp:3000)
   cd ~/AWS_VM_References
   git checkout Deployment-Automation
+  git pull
   cd webapp
   rake db:migrate
   rails s -p 3000 -b 0.0.0.0 -d
