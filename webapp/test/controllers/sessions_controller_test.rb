29f849a4d86597ad227e7fc9815a3ceae3fd5179
Brian Min <bymi15@yahoo.com>
2016-11-03 18:03:41 +0000

    merged staging to production

--- webapp/test/controllers/sessions_controller_test.rb
+++ webapp/test/controllers/sessions_controller_test.rb
@@ -1,9 +1,25 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get sessions_new_url
+  fixtures :users
+
+  test "should get login" do
+    get "/login"
     assert_response :success
   end
+
+  test "can login to app with correct details" do
+    post "/login", params: { session: { email: users(:one).email, password: 'testtest' } }
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+    assert_equal '/projects', path
+  end
+
+  test "cannot login to app with incorrect details" do
+    post "/login", params: { session: { email: users(:two).email, password: 'asdasdasda' } }
+    assert_equal '/login', path
+  end
+
 
 end
