24bd26bb8bc2c07bcc0c681fd073fe51b63ef049
Brian Min <bymi15@yahoo.com>
2016-11-03 17:44:16 +0000

    added unit tests for user registration

--- webapp/test/controllers/users_controller_test.rb
+++ webapp/test/controllers/users_controller_test.rb
@@ -6,4 +6,33 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "can register an account with valid details" do
+    post "/users", params: { user: { name: 'testuser1', email: 'testuser1@test.com', password: 'asdasd', password_confirmation: 'asdasd' } }
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+    assert_equal '/projects', path
+
+    #test database has the new user
+    assert_equal 'testuser1', User.select("name").where(:email => "testuser1@test.com").first.name
+  end
+
+  test "cannot register an account with invalid email" do
+    post "/users", params: { user: { name: 'testuser1', email: 'tasdasd12', password: 'asdasd', password_confirmation: 'asdasd' } }
+    assert_response :ok
+    assert_equal '/users', path
+  end
+
+  test "cannot register an account with invalid password" do
+    post "/users", params: { user: { name: 'testuser1', email: 'testuser1@test.com', password: 'aa', password_confirmation: 'aa' } }
+    assert_response :ok
+    assert_equal '/users', path
+  end
+
+  test "cannot register an account with non-matching passwords" do
+    post "/users", params: { user: { name: 'testuser1', email: 'testuser1@test.com', password: 'asdasd321', password_confirmation: 'asdasd123' } }
+    assert_response :ok
+    assert_equal '/users', path
+  end
+
 end
