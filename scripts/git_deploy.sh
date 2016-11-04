f63688f73c2d69163895bfa6a615f1cfc997b172
Pranav Nashikkar <pnashikkar96@gmail.com>
2016-11-04 12:00:37 +0000

    Update git_deploy.sh

--- scripts/git_deploy.sh
+++ scripts/git_deploy.sh
@@ -4,5 +4,15 @@ ssh app@51.140.162.169 <<EOF
   cd ~/AWS_VM_References
   git fetch --all
   git reset --hard origin/master
+  cd webapp
+  bundle install
+  rake db:migrate RAILS_ENV=test
+  rake test
+EOF
+
+ssh app@51.140.181.55 <<EOF
+  cd ~/AWS_VM_References
+  git fetch --all
+  git reset --hard origin/master
 EOF
 
