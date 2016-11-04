90058c5d9115f73dfa8b231dce02a7543589e88f
root <root@ProductionServer.iumdxofgcvhe5glccu5e251moe.zx.internal.cloudapp.net>
2016-11-04 12:15:25 +0000

    updated database backup scripts for production server

--- webapp/backup.sh
+++ webapp/backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-s3cmd -r put /home/teamu/AWS_VM_References/webapp/backup/ s3://database-backup-scenario-week-3/backup-$(date +%d-%m-%Y/%H:%M:%S)/ 2>&1
+/usr/local/bin/s3cmd -r put /home/app/AWS_VM_References/webapp/backup/ s3://database-backup-scenario-week-3/backup-$(date +%d-%m-%Y/%H:%M:%S)/ 2>&1
--- webapp/start_backup.sh
+++ webapp/start_backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-(crontab -l 2>/dev/null; echo "* * * * * /home/teamu/AWS_VM_References/webapp/backup.sh") | crontab -
+(crontab -l 2>/dev/null; echo "* * * * * /home/app/AWS_VM_References/webapp/backup.sh") | crontab -
 
